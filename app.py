8d3067fa4587ac6be4f98dc52c43a1a746133515
dawid mrotek <DawidMrotek@protonmail.ch>
2024-12-17 21:35:13 +0100

    [ADD] Route to upload files

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request
 from langchain_ollama import OllamaLLM
+import os
 
 
 app = Flask(__name__)
 
 cached_llm = OllamaLLM(model="llama3.2:3b")
 
-# 
-# print(response)
 
 
 @app.route("/ai", methods=["POST"])
@@ -19,6 +18,24 @@ def aiPost():
     print(f"query: {query}")
     response_answer = cached_llm.invoke(query)
     return response_answer
+
+
+
+@app.route("/pdf", methods=["POST"])
+def pdfPost():
+    file = request.files["file"]
+    file_name = file.filename
+    dir_name = "pdf/"
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name) 
+    save_file = os.path.join(dir_name, file_name)
+    file.save(save_file)
+
+    print(f"filename: {file_name}")
+    response = {"status": "Upload successfull", "filename": file_name}
+    return response
+
+
 
 def start_app():
     app.run(host="0.0.0.0", port=8080, debug=True)
