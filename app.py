2e14371ec4f8f61ef56a6f59a8b5a8111e01ef26
dawid mrotek <DawidMrotek@protonmail.ch>
2024-12-17 15:26:06 +0100

    [ADD] Route for Ollama API call

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
+from flask import Flask, request
 from langchain_ollama import OllamaLLM
 
 
-llm = OllamaLLM(model="llama3.2:3b")
+app = Flask(__name__)
 
-response = llm.invoke("Why is sky blue?")
-print(response)
+cached_llm = OllamaLLM(model="llama3.2:3b")
+
+# 
+# print(response)
+
+
+@app.route("/ai", methods=["POST"])
+def aiPost():
+    print("aiPost called")
+    json_content = request.json
+    query = json_content.get("query")
+
+    print(f"query: {query}")
+    response_answer = cached_llm.invoke(query)
+    return response_answer
+
+def start_app():
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
+
+
+if __name__ == "__main__":
+    start_app()
